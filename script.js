72bbb3a1871a24d8a6dc3b8cad1096522dfe8333
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-13 09:36:27 -0400

    Changed project from Spain Trivia Game to Spainish Trivia Game - had to rename Git repo and project folder. Think I accidentally made changes to old version yesterday so I pulled those changes into the new version - think the issue is sorted out now.

--- script.js
+++ script.js
@@ -6,8 +6,41 @@ Basic concept:
 -If answer is incorrect, user is notified that their answer is incorrect and are provided with the correct answer.
 -Computer keeps score - displays number of questions answered correctly out of total questions in game.
   --Score updates each time a question is answered.
-    ---If user answer is correct, total number of correct question increments by 1.
+    ---If user answer is correct, total number of correct questions increments by 1.
     ---If user inswer is incorrect, total number of correct questions remains unchanged.
   --Total number of questions remains the same.
 -When all questions are answered, alert user that the game is over and provide them with their final score (number correct out of total questions).
+-----Silver level: score tracking box instead of just giving the user the score as a prompt. Give user final grade report at end. Give user option to restart test.
 */
+
+$(document).ready(function(){
+
+//variables:
+  //var numCorrect = number of questions answered correctly - begins with 0 and increments by one each time question is answered correctly (numCorrect++) - inside function where we check for the correct answer
+  //var totalQuestions = total number of questions in game (question.length?)
+  //var userResponse = user's response to the questions
+  //correctAnswer = correct response to an individual question
+
+//Unclear on whether trivia questions and answers should be stored in HTML and referenced based on class/id/etc. using jQuery selectors or if everything should be stored in JS and questions shoudl be prompts like choose your own adventure game.
+
+//Need event listeners to listen for submit click
+
+//for loop to iterate through all answer options to see if userResponse matches correctAnswer
+
+//function to see if userResponse is correct or incorrect
+// function isItCorrect() {
+//var numCorrect = 0;
+// if userResponse === (correct answer)
+// alert("Correct! You have gotten " + numCorrect + "out of " + totalQuestions + "correct.")
+//numCorrect++
+// }
+// else {
+//   alert("Incorrect. The correct answer is " + (correct answer here...). "You have gotten " + numCorrect + "out of " + totalQuestions + "correct.")
+// }
+// }
+//call the function
+
+
+
+
+}
