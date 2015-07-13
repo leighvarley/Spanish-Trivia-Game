914b486ac879922682640063e32914d2c4020eae
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-12 23:06:14 -0400

    Some HTML. Lots of pseudocode.

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Self-scoring trivia game
+Basic concept:
+-Computer asks question.
+-User enters answer.
+-If answer is correct, user is notified that they answered the question correctly.
+-If answer is incorrect, user is notified that their answer is incorrect and are provided with the correct answer.
+-Computer keeps score - displays number of questions answered correctly out of total questions in game.
+  --Score updates each time a question is answered.
+    ---If user answer is correct, total number of correct question increments by 1.
+    ---If user inswer is incorrect, total number of correct questions remains unchanged.
+  --Total number of questions remains the same.
+-When all questions are answered, alert user that the game is over and provide them with their final score (number correct out of total questions).
+*/
